a5d8d9740f99a30f6626c2157f8e708416c5b532
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-08-14 23:32:03 +0200

    Banner in Homepage

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 import styles from "@/styles/Layout.module.css";
+import {useRouter} from "next/router";
 
 export default function Layout({title, description, keywords, children}) {
+	const router = useRouter();
+
 	return (
 		<div>
 			<Head>
@@ -13,6 +17,9 @@ export default function Layout({title, description, keywords, children}) {
 			</Head>
 
 			<Header />
+
+			{router.pathname === '/' && <Showcase />}
+
 			<div className={styles.container}>
 				{children}
 			</div>
