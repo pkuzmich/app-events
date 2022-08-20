e1060cb1c7258bebc2c230f853a6ce73b81a420b
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-08-20 17:22:38 +0200

    Events Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,9 @@
 import Layout from "@/components/Layout";
-import {API_URL} from "@/config/index";
+import {API_URL, PER_PAGE} from "@/config/index";
 import EventItem from "@/components/EventItem";
+import Pagination from "@/components/Pagination";
 
-export default function EventsPage({ events }) {
-
+export default function EventsPage({ events, page, total }) {
 	return (
 		<Layout>
 			<h1>Events</h1>
@@ -12,16 +12,26 @@ export default function EventsPage({ events }) {
 			{events.map(evt => (
 				<EventItem key={evt.id} evt={evt} />
 			))}
+
+			<Pagination page={page} total={total} />
 		</Layout>
 	);
 }
+// https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/rest/sort-pagination.html#pagination-by-offset
+export async function getServerSideProps({ query: {page= 1} }) {
+	// Calculate start page
+	const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
 
-export async function getStaticProps() {
-	const res = await fetch(`${API_URL}/api/events?populate=image&_sort=date:ASC&_limit=3`);
+	// Fetch events
+	const res = await fetch(`${API_URL}/api/events?populate=image&pagination[start]=${start}&pagination[limit]=${PER_PAGE}`);
 	const events = await res.json();
+	const total = events.meta.pagination.total;
 
 	return {
-		props: { events: events.data },
-		revalidate: 1,
+		props: {
+			events: events.data,
+			page: +page,
+			total
+		}
 	}
 }
