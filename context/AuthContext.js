b63dcc9a7f32333405c175cfef817365f0528ee1
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-08-23 12:10:03 +0200

    lint

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -44,7 +44,7 @@ export const AuthProvider = ({ children }) => {
 	};
 
 	return (
-		<AuthContext.Provider value={{ user, error, register, login, logout } }>
+		<AuthContext.Provider value={{ user, error, register, login, logout }}>
 			{children}
 		</AuthContext.Provider>
 	);
